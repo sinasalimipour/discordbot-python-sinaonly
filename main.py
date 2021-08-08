9f654e4b7aab4e019a0e45af8c26747fb751d283
sinagun <38187655+sinagun@users.noreply.github.com>
2021-08-09 02:23:54 +0430

    0.1

---
+++ main.py
@@ -0,0 +1,38 @@
+
+
+import discord
+import os
+import requests
+import json
+
+TOKEN= ''
+
+client = discord.Client()
+
+def get_quote():
+  resource=requests.get("https://zenquotes.io/api/random")
+
+  json_data = json.loads(resource.text)
+
+  quote= json_data[0]['q']+" -" + json_data[0]['a']
+  return(quote)
+
+
+
+
+
+@client.event
+async def on_ready():
+  print('we have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+client.run(TOKEN)
